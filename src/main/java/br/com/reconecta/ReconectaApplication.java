96807800565d3b52b43def291382cbd2e86164db
Rangel Soares <rangel.soares.souza@gmail.com>
2020-03-07 18:43:13 -0300

    initial commit

---
+++ src/main/java/br/com/reconecta/ReconectaApplication.java
@@ -0,0 +1,13 @@
+package br.com.reconecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconectaApplication.class, args);
+	}
+
+}
